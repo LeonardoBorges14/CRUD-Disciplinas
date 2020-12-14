cf4a2142ca3176859bb1ed62b079efc76266061f
leonardobt14 <leonardo3ktt@gmail.com>
2020-12-14 15:47:51 -0300

    Novos_Registros

--- src/main/java/com/leonardo/crud/controle/DisciplinasControle.java
+++ src/main/java/com/leonardo/crud/controle/DisciplinasControle.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.leonardo.crud.dao.DisciplinaDAO;
 import com.leonardo.crud.dominio.Disciplina;
 
@@ -17,5 +19,19 @@ public class DisciplinasControle {
 		modelo.addAttribute("lista",lista);
 		return "disciplinas"; 
 	}
+	
+	@GetMapping("/cadastro")
+	public String mostraFormulario(Model modelo) {
+		modelo.addAttribute("disciplina", new Disciplina());
+		return "form-disciplina";
+	}
+		
+	@PostMapping("/cadastro")
+	public String processaFormulario(Disciplina disciplina) {
+	DisciplinaDAO dao = new DisciplinaDAO();
+	dao.inserir(disciplina);
+		return "redirect:/disciplinas";
+	}
+
 
 }
